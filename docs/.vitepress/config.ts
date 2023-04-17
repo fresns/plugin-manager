3595f17de833e71dfb6d4861af513c8c15c34a0a
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-04-18 00:42:56 +0800

    feat(docs): search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -72,6 +72,31 @@ export default defineConfig({
             }
         ],
 
+        search: {
+            provider: 'local',
+            options: {
+                locales: {
+                    'zh-Hans': {
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                noResultsText: '无法找到相关结果',
+                                resetButtonTitle: '清除查询条件',
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                    closeText: '关闭',
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
         socialLinks: [
             { icon: 'github', link: 'https://github.com/fresns/plugin-manager' }
         ]
