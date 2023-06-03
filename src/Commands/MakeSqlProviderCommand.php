dadc1456cbe692c3d5d5ea3fbf15f587e10cbf1e
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-06-03 13:32:54 +0800

    chore: stub templates

---
+++ src/Commands/MakeSqlProviderCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jevan Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace Fresns\PluginManager\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeSqlProviderCommand extends GeneratorCommand
+{
+    use Traits\StubTrait;
+
+    protected $signature = 'make:sql-provider {fskey=SqlLogServiceProvider}';
+
+    protected $description = 'Generate a sql log service provider for specified plugin and need your manual reigster it.';
+
+    public function handle()
+    {
+        $path = $this->getPath('Providers/'.$this->getNameInput());
+        $pluginFskey = basename(dirname($path, 3));
+        $pluginJsonPath = dirname($path, 3) . '/plugin.json';
+
+        parent::handle();
+
+        $this->replaceInFile(
+            $this->getPluginJsonSearchContent($pluginFskey),
+            $this->getPluginJsonReplaceContent($this->getNameInput(), $pluginFskey),
+            $pluginJsonPath,
+        );
+    }
+
+    protected function getStubName(): string
+    {
+        return 'sql-provider';
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace."\Providers";
+    }
+}
