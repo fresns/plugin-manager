cac5a7c0fce9d2af6d4aca4622f6645c116b66f1
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-10-24 22:18:52 +0800

    feat: http proxy

--- src/Commands/PluginComposerUpdateCommand.php
+++ src/Commands/PluginComposerUpdateCommand.php
@@ -19,7 +19,12 @@ class PluginComposerUpdateCommand extends Command
 
     public function handle()
     {
+        $httpProxy = config('app.http_proxy');
+
         $process = Process::run(<<<"SHELL"
+            export http_proxy=$httpProxy https_proxy=$httpProxy
+            echo http_proxy=\$http_proxy
+            echo https_proxy=\$https_proxy
             echo "current user:" `whoami`
             echo "home path permission is:" `ls -ld ~`
             echo ""
